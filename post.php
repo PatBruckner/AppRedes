149cdbd19fefe6ff53628427551bda2f08a3fa7f
jordiugarte <jordi933@gmail.com>
2020-11-04 00:04:10 -0400

    Create post.php

---
+++ post.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = "localhost";
+    $username = "jugarte18";
+    $password = "030899jm";
+    $dbname = "jugarte18";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $id_message = $_GET['id_message'];
+    $id_user = $_GET['id_user'];
+    $text_message = $_GET['text_message'];
+
+    $sql = "INSERT INTO messages (id_message, id_user, text_message) VALUES ($id_message, $id_user, $text_message)";
+    $result = $conn->query($sql);
+?>

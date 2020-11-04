bd0ab66eec1b0e97dfeddc6364c6608e3d720936
jordiugarte <jordi933@gmail.com>
2020-11-04 00:18:41 -0400

    Update post.php

--- post.php
+++ post.php
@@ -10,6 +10,11 @@
     $id_user = $_GET['id_user'];
     $text_message = $_GET['text_message'];
 
-    $sql = "INSERT INTO messages (id_message, id_user, text_message) VALUES ($id_message, $id_user, $text_message)";
-    $result = $conn->query($sql);
+    if (isset($_GET['id_message']) && isset($_SESSION['id_user']) && isset($_SESSION['text_message'])) {
+        $sql = "INSERT INTO messages (id_message, id_user, text_message) VALUES ($id_message, $id_user, $text_message)";
+        $result = $conn->query($sql);
+    } else {
+        echo 'Error';
+    }
+    
 ?>
